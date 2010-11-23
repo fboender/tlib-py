7bd769e027544b6cbedc421e359bcd80afa5500c
todsah <todsah@cef0956b-0945-4077-bb19-92ab20da8b52>
2010-11-23 06:19:34 +0000

    New version of SDB (SimpleDB).
    git-svn-id: https://svn.electricmonk.nl/svn/tlib-py/trunk@8 cef0956b-0945-4077-bb19-92ab20da8b52

--- sdb.py
+++ sdb.py
@@ -1,41 +1,156 @@
-import sys
-import shlex
-
-"""
-"""
+#!/usr/bin/python
 
 class SDB(object):
-	def __init__(self, colnames, coltypes=None):
-		self.colnames = colnames
-		self.coltypes = coltypes
-		self.rows = []
+	"""
+	SimpleDB
 
-	def read_file(self, fname, sep="\t", errors=True):
-		for line in file(fname, 'r'):
-			cols = line.strip().split(sep)
+	Examples:
+	>>> data = [
+	...   ['tom', 18, 'Student'],
+	...   ['dick', 65, 'Retired'],
+	...   ['harry', 30, 'Unemployed'],
+	...   ['seymore', 20, 'Student'],
+	... ]
+	>>> sdb = SDB(data=data, cols=['name', 'age', 'occupation'], id='name')
+	>>> sdb.get('tom')
+	{'age': 18, 'name': 'tom', 'occupation': 'Student'}
+	>>> sdb.select(lambda row: row['occupation'] == 'Student')
+	[{'age': 18, 'name': 'tom', 'occupation': 'Student'}, {'age': 20, 'name': 'seymore', 'occupation': 'Student'}]
+	>>> sdb.select(lambda row: row['age'] < 65, lambda a, b: cmp(a['name'], b['name']))
+	[{'age': 30, 'name': 'harry', 'occupation': 'Unemployed'}, {'age': 20, 'name': 'seymore', 'occupation': 'Student'}, {'age': 18, 'name': 'tom', 'occupation': 'Student'}]
+	>>> sdb.save('occup.dat')
+	>>> sdb = SDB(path='occup.dat', cols=['name', 'age', 'occupation'], id='name', col_types=[str, int, str])
+	>>> for row in sdb:
+	...   print row
+	{'age': 18, 'name': 'tom', 'occupation': 'Student'}
+	{'age': 65, 'name': 'dick', 'occupation': 'Retired'}
+	{'age': 30, 'name': 'harry', 'occupation': 'Unemployed'}
+	{'age': 20, 'name': 'seymore', 'occupation': 'Student'}
+	>>> sdb.insert(['claus', 350, 'Jolly man'])
+	>>> sdb.select(lambda row: row['age'] > 60 or 't' in row['name'])
+	[{'age': 18, 'name': 'tom', 'occupation': 'Student'}, {'age': 65, 'name': 'dick', 'occupation': 'Retired'}, {'age': 350, 'name': 'claus', 'occupation': 'Jolly man'}]
+	>>> sdb.update({'age': 20, 'occupation': 'Junior Developer'}, lambda row: row['name'] == 'tom')
+	>>> sdb.get('tom')
+	{'age': 20, 'name': 'tom', 'occupation': 'Junior Developer'}
+	>>> sdb.delete(lambda row: row['age'] < 30)
+	>>> sdb.select()
+	[{'age': 65, 'name': 'dick', 'occupation': 'Retired'}, {'age': 30, 'name': 'harry', 'occupation': 'Unemployed'}, {'age': 350, 'name': 'claus', 'occupation': 'Jolly man'}]
+	>>> os.unlink('occup.dat')
+	"""
+	def __init__(self, path=None, data=None, sep=',', id=None, cols=[], col_types=None):
+		self.path = path
+		self.sep = sep
+		self.cols = cols
+		self.id = id
+		self.col_types = col_types
 
-			# Does the line has the expected number of columns?
-			if len(cols) != len(self.colnames):
-				if errors:
-					raise SDBError('Invalid number of cols in line "%s"' % (line))
+		self.data = []
+		self.id_key = None
+
+		# If user specified data as a parameter, set it. Otherwise, if the user
+		# specified a path to a file, read data from that file.
+		if data:
+			self.data = data
+		elif path:
+			self.load(path)
+		else:
+			self.data = []
+
+		# Resolve the id
+		if id:
+			try:
+				self.id_index = self.cols.index(id)
+			except ValueError:
+				self.id_index = id
+		else:
+			self.id = 0
+			self.id_index = 0
+
+	def load(self, path):
+		"""
+		Load data from a file. Uses `self.sep` and `self.col_types`.
+		"""
+		self.data = []
+		with open(path) as f:
+			for line in f:
+				cols = line.rstrip().split(self.sep)
+				if self.col_types:
+					self.data.append([f(x) for f, x in zip(self.col_types, cols)])
 				else:
-					continue # Skip this line
+					self.data.append(cols)
 
-			self.rows.append(cols)
-		
-	def query(self, fields, query):
-		lex = shlex.shlex(query)
-		token_queue = []
-		for token in lex:
-			if token.lower() == '=':
-				field = token_queue.pop()
-				value = token.get_token()
+	def save(self, path=None):
+		"""
+		Save data to a file. Uses `self.sep` and `self.col_types`.
+		"""
+		if not path:
+			path = self.path
+		else:
+			self.path = path
 
-				print "IS"
-			else:
-				token_queue.append(token)
-				
-s = SDB(('username', 'passwd', 'uid', 'gid', 'fullname', 'homedir', 'shell'))
-s.read_file('/etc/passwd', sep=':')
-s.query(('username', 'uid'), 'shell = "/bin/false"')
+		if not path:
+			raise IOError("Invalid filename '%s'" % (path))
 
+		with file(path, 'w') as f:
+			for row in self.data:
+				f.write(self.sep.join([str(col) for col in row]) + '\n')
+
+	def get(self, id):
+		"""
+		Get a row by its id (`self.id`)
+		"""
+		for row in self.data:
+			if row[self.id_index] == id:
+				return(self._make_drow(row))
+
+	def select(self, select_cb=None, sort_cb=None):
+		"""
+		Select data using a callback function `select_cb` which should return
+		True or False if the row should be included in the results. `sort_cb`
+		can be used as a sorting callback. See help documentation for the SDB
+		class for examples.
+		"""
+		results = []
+		for row in self.data:
+			drow = self._make_drow(row)
+			if not select_cb or select_cb(drow):
+				results.append(drow)
+		if sort_cb:
+			results.sort(sort_cb)
+		return(results)
+
+	def insert(self, values):
+		self.data.append(values)
+
+	def update(self, values, select_cb):
+		for i in range(0, len(self.data)):
+			row = self.data[i]
+			drow = self._make_drow(row)
+			if select_cb(drow):
+				for key, value in values.items():
+					if self.cols:
+						row[self.cols.index(key)] = value
+					else:
+						row[key] = value
+
+	def delete(self, select_cb):
+		for i in range(len(self.data), 0, -1):
+			if select_cb(self._make_drow(self.data[i-1])):
+				self.data.pop(i-1)
+
+	def _make_drow(self, row):
+		"""
+		Turn a row into a dictionary mapping if column names are available.
+		"""
+		if self.cols:
+			return(dict(zip(self.cols, row)))
+		else:
+			return(row)
+
+	def __iter__(self):
+		for row in self.data:
+			yield self._make_drow(row)
+
+if __name__ == '__main__':
+	import doctest, os
+	doctest.testmod()
