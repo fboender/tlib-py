494b1d3a5ab20200a457e835d2df99bcf4caaa3e
todsah <todsah@cef0956b-0945-4077-bb19-92ab20da8b52>
2010-12-01 18:19:21 +0000

    Added license information to each file.
    git-svn-id: https://svn.electricmonk.nl/svn/tlib-py/trunk@11 cef0956b-0945-4077-bb19-92ab20da8b52

--- typed.py
+++ typed.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2010 Ferry Boender
+# 
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 def typed(*d_args, **d_kwargs):
 	"""
 	Function parameter typing decorator. Raises TypeError if a passed parameter
@@ -24,7 +38,7 @@ def typed(*d_args, **d_kwargs):
 			return(fn(*args, **kwargs))
 		return(new)
 	return(decorator)
-		
+
 if __name__ == '__main__':
    import doctest
    doctest.testmod(optionflags=doctest.IGNORE_EXCEPTION_DETAIL)
