0b6d027efbbb4bfe6534867e4ca59fefed6e715c
todsah <todsah@cef0956b-0945-4077-bb19-92ab20da8b52>
2010-12-20 10:09:26 +0000

    Added PDO, a simple database object wrapper.
    git-svn-id: https://svn.electricmonk.nl/svn/tlib-py/trunk@13 cef0956b-0945-4077-bb19-92ab20da8b52

---
+++ pdo.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+"""
+Simple Database layer that can return rows as dicts.
+"""
+
+import sys
+
+class PDOError(Exception):
+	def __init__(self, message, code):
+		self.code = code
+		Exception.__init__(self, message)
+
+class PDO:
+	debug = False
+
+	def __init__(self, engine, **kwargs):
+		self.engine = engine
+
+		try:
+			if engine == 'sqlite3':
+				self.module = __import__('sqlite3')
+				self.connection = self.module.connect(kwargs['db'])
+			elif engine == 'MySQLdb':
+				self.module = __import__('MySQLdb')
+				self.connection = self.module.connect(**kwargs)
+			else:
+				raise PDOError('Unsupported engine \'%s\'' % (engine), 1)
+		except KeyError, e:
+			raise PDOError('Missing parameter for engine connect: %s' % (e.message), 2)
+
+	def execute(self, query, *args, **kwargs):
+		cursor = PDOCursor(self.connection.cursor())
+		if self.debug:
+			sys.stderr.write('%s\n' % query)
+			for v in args:
+				sys.stderr.write('%s\n' % str(v))
+			for k, v in kwargs:
+				sys.stderr.write('%s:%s\n' % (k, str(v)))
+		cursor.execute(query, *args, **kwargs)
+		return(cursor)
+
+	def find(self, table, **kwargs):
+		qry_where = ' AND '.join(['%s=:%s' % (key, key) for key in kwargs.keys()])
+		query = "SELECT * FROM `%s` WHERE %s" % (table, qry_where)
+		return(self.execute(query, kwargs))
+
+	def commit(self):
+		self.connection.commit()
+
+class PDOCursor:
+
+	def __init__(self, cursor):
+		self.cursor = cursor
+
+	def __getattr__(self, x):
+		return(getattr(self.cursor, x))
+
+	def fetch(self):
+		row = self.cursor.fetchone()
+		if row:
+			return(dict([(coldesc[0], row[pos]) for pos, coldesc in enumerate(self.description)]))
+		else:
+			return None
+		
+	def __iter__(self):
+		while True:
+			row = self.fetch()
+			if not row:
+				break
+			yield(row)
+
+	def __str__(self):
+		return('<PDOCursor object at %s>' % (id(self)))
+
+if __name__ == "__main__":
+	# Self tests
+	import os
+
+	#try:
+	#	os.unlink('pdo.db')
+	#except OSError:
+	#	pass
+
+	#try:
+	#	pdo = PDO('unknownengine')
+	#except PDOError, e:
+	#	if e.code == 1:
+	#		pass
+	#	else:
+	#		raise
+
+	#try:
+	#	pdo = PDO('sqlite3')
+	#except PDOError, e:
+	#	if e.code == 2:
+	#		pass
+	#	else:
+	#		raise
+
+	pdo = PDO('sqlite3', db='pdo.db')
+	#res = pdo.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, name VARCHAR(100), value INTEGER)')
+	#res = pdo.execute('INSERT INTO test VALUES (NULL, :name, :value)', {'name':'foo', 'value':101})
+	#pdo.commit()
+
+	res1 = pdo.execute('SELECT id FROM test')
+	print res1
+	for row in res1:
+		print row
